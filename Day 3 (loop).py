0a09ee5015ea2595785d00c1c883424eb88b6e5a
Drishya <60887573+drishya-lol@users.noreply.github.com>
2020-08-26 18:29:57 +0545

    Delete Day 3 (loop).py

--- Day 3 (loop).py
+++
@@ -1,8 +0,0 @@
-intry = int(input("Enter the number whose multiplication table you want: "))
-maxVal = int(input("Enter the maximum limit: "))
-i=1
-
-while i <= maxVal:
-    answers =  intry * i
-    print(str(intry) , "*" , i , "=" , answers)
-    i += 1
